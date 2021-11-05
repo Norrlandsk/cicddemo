948a1a73569c50bc5dd674a4fee7590510e0337c
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-11-05 10:59:13 +0100

    initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cicddemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
